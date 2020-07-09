2712cef50382457d1c328a800e53f9a7480a0120
Ali Moussa <imalimoussa@gmail.com>
2020-07-09 14:58:43 +0200

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,37 +1,64 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
+            selectedDish: null
+        };
+    }
 
-  onDishSelect(dishId) {
-    console.log(dishId);
-    this.setState({ selectedDish: dishId });
-  }
+    onDishSelect(dishId) {
+        alert(dishId);
+        this.setState({ selectedDish: dishId });
+    }
 
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
-    );
-  }
+    render() {
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        };
+
+        return (
+            <React.Fragment>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route path='/contactus' component={Contact} />
+                    <Route exact path='/menu'>
+                        <React.Fragment>
+                            <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                        </React.Fragment>
+                    </Route>
+                    <Redirect to='/home' />
+                </Switch>
+
+                <Footer />
+            </React.Fragment>
+        );
+    }
 }
 
 export default Main;
